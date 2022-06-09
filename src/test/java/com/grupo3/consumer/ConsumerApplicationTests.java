baa2947cc686cbcf7ca99697aac91ca252fe8aea
debora.brum <debrumneves@gmail.com>
2022-06-09 12:17:23 -0300

    feat(back): tests develop

--- src/test/java/com/grupo3/consumer/ConsumerApplicationTests.java
+++ src/test/java/com/grupo3/consumer/ConsumerApplicationTests.java
@@ -6,6 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class ConsumerApplicationTests {
 
+	private final static String TEST_CONFIG_FILE = "configuration/test.properties";
+
 	@Test
 	void contextLoads() {
 	}
